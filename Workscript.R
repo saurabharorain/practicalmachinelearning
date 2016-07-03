877e3d9712afcdb02ff946c9b8be6bd846e1e501
Saurabh Arora <saurabh_arorain@yahoo.com>
2016-07-03 13:56:07 +0530

    update with index.html

--- Workscript.R
+++ Workscript.R
@@ -1,58 +1,78 @@
 
+
 library("caret")
 library("dplyr")
 library(doMC)
 registerDoMC(cores = 4)
 
 
-if (!file.exists("./pml-training.csv")){
- 
-    fileurl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
-    download.file(fileurl,"./pml-training.csv",method="curl")
+if (!file.exists("./pml-training.csv")) {
+  fileurl <-
+    "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
+  download.file(fileurl, "./pml-training.csv", method = "curl")
 }
 
-if (!file.exists("./pml-testing.csv")){
-  
-  fileurl <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
-  download.file(fileurl,"./pml-testing.csv",method="curl")
+if (!file.exists("./pml-testing.csv")) {
+  fileurl <-
+    "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+  download.file(fileurl, "./pml-testing.csv", method = "curl")
 }
 
-trainset <- read.csv(file="./pml-training.csv",header = TRUE);
-testset  <- read.csv(file="./pml-testing.csv",header = TRUE);
+trainset <- read.csv(file = "./pml-training.csv", header = TRUE)
 
-removeredunt <- trainset %>% select(-X,-raw_timestamp_part_1,-raw_timestamp_part_2,-cvtd_timestamp,-new_window,-num_window)
+testset  <- read.csv(file = "./pml-testing.csv", header = TRUE)
+
+
+removeredunt <-
+  trainset %>% select(
+    -X,
+    -raw_timestamp_part_1,
+    -raw_timestamp_part_2,
+    -cvtd_timestamp,
+    -new_window,
+    -num_window,
+    -user_name,
+    -classe
+  )
+removeredunt$classe <- as.factor(trainset$classe)
+
 #remove varible with all NA
 withoutallna <- removeredunt[, colSums(is.na(removeredunt)) == 0]
 dim(withoutallna)
 
-nearzero <- nearZeroVar(withoutallna,saveMetrics = TRUE)
-cleaned <- withoutallna[,nearzero[,"nzv"]==FALSE]
+nearzero <- nearZeroVar(withoutallna, saveMetrics = TRUE)
+cleaned <- withoutallna[, nearzero[, "nzv"] == FALSE]
 dim(cleaned)
 
 #default seed
 set.seed(1234)
 
-inTraining <- createDataPartition(cleaned$classe, p = .7, list=FALSE)
+inTraining <-
+  createDataPartition(cleaned$classe, p = .7, list = FALSE)
 
-trainsetwithoutna <- cleaned[inTraining,]
-validation <- cleaned[-inTraining,]
+trainsetwithoutna <- cleaned[inTraining, ]
+validation <- cleaned[-inTraining, ]
 dim(trainsetwithoutna)
 
-pcadata <- preProcess(trainsetwithoutna %>% select(-classe),method="pca")
-processtrainset <- predict(pcadata,trainsetwithoutna %>% select(-classe))
-
-dim(processtrainset)
 
 start <- Sys.time()
 set.seed(1234)
-model <- train(trainsetwithoutna$classe~.,data=processtrainset,method="rf",verbose=TRUE,allowparallel=TRUE,ntree=100,trControl=trainControl(method="cv",number="2"),prox=TRUE);
+model <-
+  train(
+    classe ~ .,
+    data = trainsetwithoutna,
+    method = "rf",
+    verboseIter = TRUE,
+    allowparallel = TRUE,
+    prox = TRUE
+  )
+
+#trControl = trainControl(method = "cv", number = "2"), ntree = 100,   preProcess = c("pca"),
 end <- Sys.time()
 
-end - start 
+end - start
 
 confusionMatrix(model)
+confusionMatrix(predict(model,validation),validation$classe)
 
-saveRDS(model,file="baserf.rds")
-
-
-
+saveRDS(model, file = "baserf.rds")
